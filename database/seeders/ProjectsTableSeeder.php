3850cafed6620860c62b3be9aebc85f0b9dbfd0b
valerioperria <valerio.perria@gmail.com>
2024-01-23 19:33:09 +0100

    feat. migration; seeder; model

---
+++ database/seeders/ProjectsTableSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Project;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+use Illuminate\Support\Str;
+
+class ProjectsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Faker $faker)
+    {
+        for ($i = 0; $i < 10; $i++) {
+            $project = new Project();
+            $project->title = $faker->sentence(10);
+            $project->content = $faker->text(500);
+            $project->comment = $faker->text(300);
+            $project->slug = Str::slug($project->title);
+            $project->save();
+        }
+    }
+}
