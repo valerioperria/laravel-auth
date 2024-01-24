67993f6b59f9eae8101bb84b06081addeb31753a
valerioperria <valerio.perria@gmail.com>
2024-01-24 22:05:06 +0100

    feat. soft_delete

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -5,10 +5,13 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Str;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Project extends Model
 {
     use HasFactory;
+
+    use SoftDeletes;
 
     protected $fillable = ['title', 'content', 'comment', 'slug'];
 
